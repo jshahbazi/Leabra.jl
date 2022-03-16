d1ed8f60b610b88e3b6fecca305770d569ee564f
John Shahbazian <john.shahbazian@ceros.com>
2022-03-16 08:54:37 -0400

    Add overall leabra test function, clean up layer function test

--- src/Leabra.jl
+++ src/Leabra.jl
@@ -855,15 +855,130 @@ function test_nxx1(xp::NXX1Parameters = nxx1p)
     end
 end
 
-# test_nxx1()
-# mylayer = layer((1,1))
-# activities(mylayer)
-# acts_avg(mylayer)
-# scaled_acts(mylayer)
-# averages(mylayer)
-# rel_avg_l(mylayer)
-# updt_avg_l(mylayer)
-# updt_long_avgs(mylayer)
-# reset(mylayer)
+function test_layer()
+    mylayer = layer((1,1))
+    activities(mylayer)
+    acts_avg(mylayer)
+    scaled_acts(mylayer)
+    averages(mylayer)
+    rel_avg_l(mylayer)
+    updt_avg_l(mylayer)
+    updt_long_avgs(mylayer)
+    reset(mylayer)
+end
+
+function test_network()
+    # # 1.1) Set the dimensions of the layers
+    dim_lays = [(5,5), (10, 10), (5, 5)]
+    # # 1.2) Specify connectivity between layers
+    connections = [0  0  0;
+                1  0 .2;
+                0  1  0];
+    #    # connections(i,j) = c means that layer i receives connections from j,
+    #    # and that they have a relative strength c. In this case, feedback
+    #    # connections are 5 times weaker. The relative weight scale of layer i
+    #    # comes from the non-zero entries of row i.   
+    #    # The network constructor will normalize this matrix so that if there
+    #    # are non-zero entries in a row, they add to 1.
+    n_lays = length(dim_lays);
+    n_units = zeros(Int64,1,n_lays); ## number of units in each layer
+
+    for i in 1:n_lays
+        n_units[i] = dim_lays[i][1] * dim_lays[i][2];
+    end
+
+    w0 = Array{Any}(undef, (n_lays,n_lays))
+    for rcv in 1:n_lays
+        for snd in 1:n_lays
+            if connections[rcv,snd] > 0
+                w0[rcv,snd] = 0.3 .+ 0.4*rand(Uniform(),n_units[rcv],n_units[snd]);
+            else
+                w0[rcv,snd] = 0.0
+            end
+        end
+    end
+
+    net = network(dim_lays, connections, w0);
+
+    # ## 4) Let's create some inputs
+    # n_inputs = 15;  # number of input-output patterns to associate
+    # patterns = Array{Matrix{Float64}, 2}(undef, (n_inputs,2)) # patterns{i,1} is the i-th input pattern, and 
+    #                                                           # patterns{i,2} is the i-th output pattern.
+    # # This will assume that layers 1 and 3 are input and output respectively.
+    # # Patterns will be binary.
+    # prop = 0.3; # the proportion of active units in the patterns
+    # for i in 1:n_inputs
+    #     # rand(Uniform(),n_units[rcv],n_units[snd]);
+    #     patterns[i,1] = round.(2 .* prop .* rand(Uniform(), 1, n_units[1]));  # either 0 or 1
+    #     patterns[i,2] = round.(2 .* prop .* rand(Uniform(), 1, n_units[3]));
+    #     patterns[i,1] = 0.01 .+ 0.95 .* patterns[i,1];  # either 0.01 or 0.96
+    #     patterns[i,2] = 0.01 .+ 0.95 .* patterns[i,2];
+    # end
+
+
+    n_inputs = 5;  # number of input-output patterns to associate
+    patterns = Array{Matrix{Float64}, 2}(undef, (n_inputs,2)) # patterns{i,1} is the i-th input pattern, and 
+                                                            # patterns{i,2} is the i-th output pattern.
+    # This will assume that layers 1 and 3 are input and output respectively.
+    # Pattern values are binary, either 0.01 or 0.99
+    patterns[1,1] = repeat([0 0 1 0 0],5,1);   # vertical line
+    patterns[2,1] = [1 1 1 1 1;zeros(4,5)];   # horizontal line
+    patterns[3,1] = [0 0 0 0 1;0 0 0 1 0;0 0 1 0 0;0 1 0 0 0; 1 0 0 0 0]; # diagonal 1
+    patterns[4,1] = reverse(patterns[3,1], dims=2) #patterns[3,1][[5 4 3 2 1],:]   # diagonal 2
+    patterns[5,1] =  1.0 .* (!=(0.0).(patterns[3,1]) .|| !=(0.0).(patterns[4,1]))   # two diagonals
+    for i in 1:n_inputs # outputs are the same as inputs (an autoassociator)
+        patterns[i,1] = 0.01 .+ 0.98 .* patterns[i,1];
+        patterns[i,2] = patterns[i,1];
+    end
+
+    ## 5) Train the network
+
+    # Specify parameters for training
+    n_epochs = 10;  # number of epochs. All input patterns are presented in one.
+    n_trials = n_inputs; # number of trials. One input pattern per trial.
+    n_minus = 50;  # number of minus cycles per trial.
+    n_plus = 25; # number of plus cycles per trial.
+    lrate_sched = collect(LinRange(0.8, 0.2, n_epochs))
+
+    errors = zeros(n_epochs,n_trials); # cosine error for each pattern
+
+    for epoch in 1:n_epochs
+        order = randperm(n_trials); # order of presentation of inputs this epoch
+        net.lrate = lrate_sched[epoch]; # learning rate for this epoch
+        for trial in 1:n_trials
+            reset(net);  # randomize the acts for all units
+            pat = order[trial];  # input to be presented this trial
+
+            #++++++ MINUS PHASE +++++++
+            inputs::Vector{Array{Float64}} = [patterns[pat, 1], [], []];
+            for minus in 1:n_minus # minus cycles: layer 1 is clamped
+                cycle(net, inputs, true);
+            end
+            outs = (activities(net.layers[3])) # saving the output for testing
+
+            #+++++++ PLUS PHASE +++++++
+            inputs = [patterns[pat, 1], [], patterns[pat, 2]];
+            for plus in 1:n_plus # plus cycles: layers 1 and 3 are clamped
+                cycle(net, inputs, true);
+            end
+            updt_long_avgs(net) # update averages used for net input scaling                            
+
+            #+++++++ LEARNING +++++++
+            XCAL_learn(net)  # updates the avg_l vars and applies XCAL learning
+            
+            if mod(trial, 5) == 0 # display a message every X trials
+                println("Trial $trial finished")
+            end        
+            
+            #+++++++ ERRORS +++++++
+            # Only the cosine error is used here
+            errors[epoch, pat] = 1 - sum(outs .* transpose(patterns[pat, 2][:])) / ( norm(outs) * norm(transpose(patterns[pat, 2][:])) );                    
+        end
+    end
+
+    mean_errs = mean(errors, dims=2);
+    println("Errors: $mean_errs")
+    @assert all(0.0 .< mean_errs) &&  all(mean_errs .< 1.0) "Errors are out of range"
+end
 
 end # module Leabra
