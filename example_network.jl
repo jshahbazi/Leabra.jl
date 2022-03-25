d83cde37a68b84157b7c23e657dcb03623a4906a
John Shahbazian <john.shahbazian@ceros.com>
2022-03-24 22:04:10 -0400

    Tweaks

--- example_network.jl
+++ example_network.jl
@@ -5,24 +5,35 @@ using Plots
 
 include("./src/Leabra.jl")
 
+# This is an example network designed simply be a pattern associator.
+# This is pretty much the example from the Leabra Matlab code by Sergio Verduzco-Flores.
 
+# 3 layers and their dimensions
 dim_lays = [(5 , 5), 
             (10, 10), 
             (5 , 5)]
+
+# connections[i,j] = c
+# Layer i receives connections from layer j, and they have a relative strength c.
+# The network constructor will normalize this matrix so that if there
+# are non-zero entries in a row, they add to 1.            
+# 2 receives connections from 1 at relative strength of 1
+# 2 receives connections from 3 at relative strength of 0.2
+# 3 receives connections from 2 at relative strength of 1
 connections = [0  0  0;
-                1  0 .2;
-                0  1  0]
+               1  0 .2;
+               0  1  0]
 
 net = Leabra.build_network(dim_lays, connections)
 
 n_inputs = 5
+# this just creates random patterns
 inputs = Leabra.create_random_inputs(n_inputs)
 
-n_epochs = 10
-n_trials = n_inputs
-errors = Leabra.train_network!(net, inputs, n_epochs, n_trials)
-
+n_epochs = 20
+errors = Leabra.train_network!(net, inputs, n_epochs)
 
 mean_errs = mean(errors, dims=2);
 println(mean_errs)
 plot(mean_errs)
+ylims!((0.0,1.0))
